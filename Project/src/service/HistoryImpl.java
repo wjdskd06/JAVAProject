a26d29ac53072fa0c2ba08e0fb1d8d5d1bb65bcc
User <User@YD02-12>
2019-08-13 14:04:34 +0900

    08131400

---
+++ Project/src/service/HistoryImpl.java
@@ -0,0 +1,46 @@
+package service;
+
+import java.util.List;
+
+import model.History;
+import model.HistoryService;
+
+public class HistoryImpl implements HistoryService{
+
+	@Override
+	public boolean insert(History history) {
+		
+		return false;
+	}
+
+	@Override
+	public boolean update(History history) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean delete(int history) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public History selectOne(int history) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<History> selectAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public History selectOneUserId(int userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
